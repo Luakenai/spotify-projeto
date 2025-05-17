82cb1a335c9d664935da8cddc40e4a0878e66722
Lua Kenai <emily.annie413@gmail.com>
2025-05-16 22:05:20 -0300

    Revert "adjusting api"
    This reverts commit 88520146524568fe28dea8d4a3c89bf5344a9eb4.

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,9 +1,10 @@
 // Fetch ou Axios
 import axios from "axios";
+import "dotenv/config";
 
+const NODE_ENV = process.env.NODE_ENV ;
 
-
-const URL = "http://localhost:3001/api";
+const URL =  NODE_ENV === 'development' ? "http://localhost:3001/api" : "/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
